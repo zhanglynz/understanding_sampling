5996a06dbf03af241b851afde882d4b80797c921
L. Zhang <lyzhang10@gmail.com>
2019-10-12 20:31:14 +1300

    some changes

--- R/toy_example_1.R
+++ R/toy_example_1.R
@@ -1,8 +1,18 @@
 a <- c(6, 8, 8, 1, 6, 4, 7, 5, 3, 3)
 index_m <- combn(1:10, 3)
 y_bar <- rep(0, 120)
-for(i in 1:120) y_bar[i] <- mean(a[index_m[, i]])
+s2 <- rep(0, 120)
+for(i in 1:120) 
+{y_bar[i] <- mean(a[index_m[, i]])
+s2[i] <- var(a[index_m[, i]]) 
+}
 
 mean(y_bar)
 
-sd(y_bar)
+var(a) 
+
+var(y_bar) * (length(y_bar) - 1) / length(y_bar)
+
+(1-3/10) /3 * var(a) 
+
+mean(s2)
